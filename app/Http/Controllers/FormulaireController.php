207c61c628185d141e4738329f06cf6db2e3e523
christ_cross02 <christkatumba02@gmail.com>
2023-06-12 15:49:25 +0100

    Creation du controller FormulaireController cette formulaire return la vue du formulaire avec tout le donnée des utilisateur, managers et direction

---
+++ app/Http/Controllers/FormulaireController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Direction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FormulaireController extends Controller
+{
+    public function index(){
+
+        $user = Auth::user();
+        $managers = User::all();
+        $directions = Direction::all();
+
+        return view('formulaire', compact('user','managers','directions'));
+    }
+}
