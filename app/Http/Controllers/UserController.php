95bb4afdcb3906e2c18cb5fec4bbba0022db1a54
ChristKTMB <christkatumba02@gmail.com>
2023-10-26 15:27:37 +0100

    Creation du controller UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function index(){
+        $users = User::all();
+
+        return view("user.index",compact("users"));
+    }
+
+    public function edit($id){
+
+        $direction = User::find($id);  
+    }
+
+    public function update(Request $request, $id){
+        $direction = User::find($id);
+        $direction->nom = $request->input('nom');
+        $direction->save();
+
+        return redirect()->route('direction.index');
+    }
+}
